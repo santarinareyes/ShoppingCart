c4eafc0cd2f2ff48888231f0189a96802cf3b4b7
Richard <richard.santarinareyes@medieinstitutet.se>
2020-12-03 00:45:25 +0100

    target price and quant when removing from cart + backup

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,11 +1,26 @@
 // remove button
 var removeBtnCart = document.getElementsByClassName('btn-remove')
-console.log(removeBtnCart)
-
 for (var i = 0; i < removeBtnCart.length; i++) {
     var removeButton = removeBtnCart[i]
     removeButton.addEventListener('click', function(event) {
+        // -- removes parent1 and 2
         var removeClicked = event.target
-        removeClicked.parentElement.parentElement.remove() //remove parent1 och 2
+        removeClicked.parentElement.parentElement.remove()
+        // -- update Total price (updateCartTotal)
+        updateCartTotal()
     })
+}
+
+// function for (updateCartTotal)
+function updateCartTotal() {
+    //returns an array of elements - [0] = first array
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    //cart-row inside cart-items (cartItemContainer)
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var cartPrice = cartRow.getElementsByClassName('cart-price')[0]
+        var cartQuantity = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        console.log(cartPrice, cartQuantity)
+    }
 }
