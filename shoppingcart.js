bfa1b39682422aebeb44cf0ce084364cf4a8fa31
Richard <richard.santarinareyes@medieinstitutet.se>
2020-12-03 02:13:28 +0100

    added update total price and cart item-price x quantity

--- shoppingcart.js
+++ shoppingcart.js
@@ -15,12 +15,23 @@ for (var i = 0; i < removeBtnCart.length; i++) {
 function updateCartTotal() {
     //returns an array of elements - [0] = first array
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+
     //cart-row inside cart-items (cartItemContainer)
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var totalPrice = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
         var cartPrice = cartRow.getElementsByClassName('cart-price')[0]
         var cartQuantity = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        console.log(cartPrice, cartQuantity)
+
+        // innerText from .cart-price and remove the '$' from the string
+        // parseFloat = turns a string to a Float (number with decimals)
+        var price = parseFloat(cartPrice.innerText.replace('$', ''))
+
+        // get the value from cartQuantity (.cart-quantity-input)
+        // since its an <input> element we need the value from it
+        var quantity = cartQuantity.value
+        totalPrice = totalPrice + (price * quantity)
     }
+    document.getElementsByClassName('cart-total-price')[0].innerText = totalPrice + ' SEK'
 }
